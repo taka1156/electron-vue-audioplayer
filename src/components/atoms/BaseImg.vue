459a1c3b1c7bc55587784a208bd904e6d0946b46
taka <tomo05061@gmail.com>
2020-09-22 21:47:58 +0900

    fix: ナビゲーションバーをアトミックデザイン形式に修正

---
+++ src/components/atoms/BaseImg.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <img
+    :src="props.imgUrl"
+    :alt="props.imgAlt"
+    class="img"
+    :class="data.staticClass || ''"
+  />
+</template>
+
+<style scoped>
+.img {
+  display: block;
+  margin: 5px auto;
+}
+</style>
---
+++ src/components/organisms/AudioNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="NaviBar">
+    <nav>
+      <nav-list :routes="routes" />
+    </nav>
+  </div>
+</template>
+
+<script>
+import NavList from '../molecules/NavList';
+
+export default {
+  name: 'NaviBar',
+  components: {
+    'nav-list': NavList
+  },
+  props: {
+    routes: {
+      type: Array,
+      default: () => [],
+      required: true
+    }
+  }
+};
+</script>
